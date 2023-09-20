1c179fecc9c6c4ba98e5c5bdd8108b20c62b1bfd
Cyberhawks706 <cyberhawks706@gmail.com>
2023-09-19 19:09:43 -0500

    get started with folder structure

---
+++ src/main/java/org/frc706/cyberlib/subsystems/SwerveModule.java
@@ -0,0 +1,108 @@
+package org.frc706.cyberlib.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+
+
+public class SwerveModule {
+
+    private final CANSparkMax driveMotor;
+    private final CANSparkMax turningMotor;
+
+    private final RelativeEncoder driveEncoder;
+    private final RelativeEncoder turningEncoder;
+
+    private final PIDController turningPidController;
+
+
+    final AnalogInput absoluteEncoder;
+    private final double absoluteEncoderOffsetRad;
+    public final double initPos;
+
+
+    public SwerveModule(int driveMotorId, int turningMotorId, int absoluteEncoderId, double absoluteEncoderOffset) {
+
+        this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
+        absoluteEncoder = new AnalogInput(absoluteEncoderId);
+
+        driveMotor = new CANSparkMax(driveMotorId, MotorType.kBrushless);
+        turningMotor = new CANSparkMax(turningMotorId, MotorType.kBrushless);
+
+        driveEncoder = driveMotor.getEncoder();
+        turningEncoder = turningMotor.getEncoder();
+
+        driveEncoder.setPositionConversionFactor(Constants.Swerve.kDriveEncoderRot2Meter);
+        driveEncoder.setVelocityConversionFactor(Constants.Swerve.kDriveEncoderRPM2MeterPerSec);
+        turningEncoder.setPositionConversionFactor(Constants.Swerve.kTurningEncoderRot2Rad);
+        turningEncoder.setVelocityConversionFactor(Constants.Swerve.kTurningEncoderRPM2RadPerSec);
+
+        turningPidController = new PIDController(Constants.Swerve.kPTurning, 0, 0); 
+        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+
+        initPos = getAbsoluteEncoderRad();
+        resetEncoders();
+    }
+
+    public double getDrivePosition() {
+        return driveEncoder.getPosition();
+    }
+
+    public double getTurningPosition() {
+        return turningEncoder.getPosition();
+    }
+
+    public double getDriveVelocity() {
+        return driveEncoder.getVelocity();
+    }
+
+    public double getTurningVelocity() {
+        return turningEncoder.getVelocity();
+    }
+
+    public double getAbsoluteEncoderRad(){
+        double angle = absoluteEncoder.getVoltage()/RobotController.getVoltage5V();
+        angle *= 2.0*Math.PI;
+        angle -= absoluteEncoderOffsetRad;
+        return angle;
+
+
+    }
+
+    public void resetEncoders() {
+        driveEncoder.setPosition(0);
+        turningEncoder.setPosition(getAbsoluteEncoderRad());
+    }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(getDrivePosition(), new Rotation2d(getAbsoluteEncoderRad()));
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+            stop();
+            return;
+        }
+        //state.angle = state.angle.plus(new Rotation2d(absoluteEncoderOffsetRad));
+        state = SwerveModuleState.optimize(state, getState().angle);
+        driveMotor.set(state.speedMetersPerSecond / Constants.Swerve.kPhysicalMaxSpeedMetersPerSecond);
+        turningMotor.set(turningPidController.calculate(getAbsoluteEncoderRad(), state.angle.getRadians()));
+
+    }
+
+    public void stop() {
+        driveMotor.stopMotor();
+        turningMotor.stopMotor();
+    }
+}
