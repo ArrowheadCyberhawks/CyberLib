77da25619bd27d7abbc8be3afc2bb3d9bdcf333d
Cyberhawks706 <cyberhawks706@gmail.com>
2024-01-13 15:15:48 -0600

    fix path following

--- src/main/java/lib/frc706/cyberlib/subsystems/SwerveSubsystem.java
+++ src/main/java/lib/frc706/cyberlib/subsystems/SwerveSubsystem.java
@@ -16,6 +16,8 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -25,7 +27,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class SwerveSubsystem extends SubsystemBase{
+public class SwerveSubsystem extends SubsystemBase {
     private final double MAX_VELOCITY_METERS_PER_SECOND;
     AHRS gyro = new AHRS(SPI.Port.kMXP);
 
@@ -42,7 +44,8 @@ public class SwerveSubsystem extends SubsystemBase{
 
     private final ShuffleboardLayout layout;
 
-    public SwerveSubsystem(SwerveModule moduleFL, SwerveModule moduleFR, SwerveModule moduleBL, SwerveModule moduleBR, int wheelBase, HolonomicPathFollowerConfig pathFollowerConfig, PhotonCameraWrapper... cameras) {
+    public SwerveSubsystem(SwerveModule moduleFL, SwerveModule moduleFR, SwerveModule moduleBL, SwerveModule moduleBR,
+            int wheelBase, HolonomicPathFollowerConfig pathFollowerConfig, PhotonCameraWrapper... cameras) {
         layout = Shuffleboard.getTab("SwerveDrive").getLayout("SwerveDrive", BuiltInLayouts.kGrid);
         frontLeft = moduleFL;
         frontRight = moduleFR;
@@ -61,28 +64,39 @@ public class SwerveSubsystem extends SubsystemBase{
         layout.add("Field", m_field);
         recenter();
         AutoBuilder.configureHolonomic(
-            this::getPose, // Robot pose supplier
-            this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-            this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-            pathFollowerConfig,
-            this::getFlipPath,
-            this // Reference to this subsystem to set requirements
+                this::getPose, // Robot pose supplier
+                this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                pathFollowerConfig,
+                this::getFlipPath,
+                this // Reference to this subsystem to set requirements
         );
     }
-    
+
     /**
      * Creates a new SwerveSubsystem with the given parameters.
      * <p>
      * All arrays should be in the order FL, FR, BL, BR.
      * </p>
      */
-    public SwerveSubsystem(ModuleType moduleType, double wheelBase, int[] driveMotorIDs, int[] turningMotorIDs, int[] absoluteEncoderPorts, double[] absoluteEncoderOffsets, boolean[] driveMotorsInverted, boolean[] turningMotorsInverted, boolean[] absoluteEncodersInverted, HolonomicPathFollowerConfig pathFollowerConfig, PhotonCameraWrapper... cameras) {
+    public SwerveSubsystem(ModuleType moduleType, double wheelBase, int[] driveMotorIDs, int[] turningMotorIDs,
+            int[] absoluteEncoderPorts, double[] absoluteEncoderOffsets, boolean[] driveMotorsInverted,
+            boolean[] turningMotorsInverted, boolean[] absoluteEncodersInverted,
+            HolonomicPathFollowerConfig pathFollowerConfig, PhotonCameraWrapper... cameras) {
         layout = Shuffleboard.getTab("SwerveDrive").getLayout("SwerveDrive", BuiltInLayouts.kGrid);
-        frontLeft = new SwerveModule(moduleType, SwerveModule.ModulePosition.FL, driveMotorIDs[0], turningMotorIDs[0], absoluteEncoderPorts[0], absoluteEncoderOffsets[0], driveMotorsInverted[0], turningMotorsInverted[0], absoluteEncodersInverted[0]);
-        frontRight = new SwerveModule(moduleType, SwerveModule.ModulePosition.FR, driveMotorIDs[1], turningMotorIDs[1], absoluteEncoderPorts[1], absoluteEncoderOffsets[1], driveMotorsInverted[1], turningMotorsInverted[1], absoluteEncodersInverted[1]);
-        backLeft = new SwerveModule(moduleType, SwerveModule.ModulePosition.BL, driveMotorIDs[2], turningMotorIDs[2], absoluteEncoderPorts[2], absoluteEncoderOffsets[2], driveMotorsInverted[2], turningMotorsInverted[2], absoluteEncodersInverted[2]);
-        backRight = new SwerveModule(moduleType, SwerveModule.ModulePosition.BR, driveMotorIDs[3], turningMotorIDs[3], absoluteEncoderPorts[3], absoluteEncoderOffsets[3], driveMotorsInverted[3], turningMotorsInverted[3], absoluteEncodersInverted[3]);
+        frontLeft = new SwerveModule(moduleType, SwerveModule.ModulePosition.FL, driveMotorIDs[0], turningMotorIDs[0],
+                absoluteEncoderPorts[0], absoluteEncoderOffsets[0], driveMotorsInverted[0], turningMotorsInverted[0],
+                absoluteEncodersInverted[0]);
+        frontRight = new SwerveModule(moduleType, SwerveModule.ModulePosition.FR, driveMotorIDs[1], turningMotorIDs[1],
+                absoluteEncoderPorts[1], absoluteEncoderOffsets[1], driveMotorsInverted[1], turningMotorsInverted[1],
+                absoluteEncodersInverted[1]);
+        backLeft = new SwerveModule(moduleType, SwerveModule.ModulePosition.BL, driveMotorIDs[2], turningMotorIDs[2],
+                absoluteEncoderPorts[2], absoluteEncoderOffsets[2], driveMotorsInverted[2], turningMotorsInverted[2],
+                absoluteEncodersInverted[2]);
+        backRight = new SwerveModule(moduleType, SwerveModule.ModulePosition.BR, driveMotorIDs[3], turningMotorIDs[3],
+                absoluteEncoderPorts[3], absoluteEncoderOffsets[3], driveMotorsInverted[3], turningMotorsInverted[3],
+                absoluteEncodersInverted[3]);
         swerveModules = new SwerveModule[] { frontLeft, frontRight, backLeft, backRight };
         MAX_VELOCITY_METERS_PER_SECOND = moduleType.getMaxVelocity();
         updatePositions();
@@ -96,13 +110,13 @@ public class SwerveSubsystem extends SubsystemBase{
         layout.add("Field", m_field);
         recenter();
         AutoBuilder.configureHolonomic(
-            this::getPose, // Robot pose supplier
-            this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-            this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
-            pathFollowerConfig,
-            this::getFlipPath,
-            this // Reference to this subsystem to set requirements
+                this::getPose, // Robot pose supplier
+                this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                pathFollowerConfig,
+                this::getFlipPath,
+                this // Reference to this subsystem to set requirements
         );
     }
 
@@ -123,7 +137,16 @@ public class SwerveSubsystem extends SubsystemBase{
     }
 
     public boolean getFlipPath() {
-        return false;
+        Optional<Alliance> ally = DriverStation.getAlliance();
+        if (ally.isPresent()) { //check if alliance is red or blue
+            if (ally.get() == Alliance.Red) {
+                return true;
+            }
+            if (ally.get() == Alliance.Blue) {
+                return false;
+            }
+        }
+        return false; //if we don't know then just give up and say no
     }
 
     public void resetOdometry(Pose2d pose) {
@@ -144,10 +167,10 @@ public class SwerveSubsystem extends SubsystemBase{
 
     public SwerveModuleState[] getModuleStates() {
         return new SwerveModuleState[] {
-            frontLeft.getState(),
-            frontRight.getState(),
-            backLeft.getState(),
-            backRight.getState()
+                frontLeft.getState(),
+                frontRight.getState(),
+                backLeft.getState(),
+                backRight.getState()
         };
     }
 
@@ -158,18 +181,21 @@ public class SwerveSubsystem extends SubsystemBase{
     /**
      * move the robot
      * 
-     * @param xSpeed forwards speed, positive is away from our alliance wall
-     * @param ySpeed sideways speed, positive is left
-     * @param rot rotation speed, positive is counterclockwise
+     * @param xSpeed        forwards speed, positive is away from our alliance wall
+     * @param ySpeed        sideways speed, positive is left
+     * @param rot           rotation speed, positive is counterclockwise
      * @param fieldRelative whether the xSpeed and ySpeed are relative to the field
      */
     public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-        SwerveModuleState[] moduleStates = kDriveKinematics.toSwerveModuleStates(fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, getRotation2d()) : new ChassisSpeeds(xSpeed, ySpeed, rot));
+        SwerveModuleState[] moduleStates = kDriveKinematics.toSwerveModuleStates(
+                fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, getRotation2d())
+                        : new ChassisSpeeds(xSpeed, ySpeed, rot));
         setModuleStates(moduleStates);
     }
 
     /**
      * set the module states
+     * 
      * @param speeds robot-relative chassis speeds
      */
     public void driveRobotRelative(ChassisSpeeds speeds) {
@@ -184,7 +210,8 @@ public class SwerveSubsystem extends SubsystemBase{
         for (PhotonCameraWrapper camera : cameras) {
             Optional<EstimatedRobotPose> result = camera.getEstimatedGlobalPose(getPose());
             if (result.isPresent()) {
-                poseEstimator.addVisionMeasurement(result.get().estimatedPose.toPose2d(), result.get().timestampSeconds);
+                poseEstimator.addVisionMeasurement(result.get().estimatedPose.toPose2d(),
+                        result.get().timestampSeconds);
             }
         }
         m_field.setRobotPose(getPose());
@@ -192,26 +219,26 @@ public class SwerveSubsystem extends SubsystemBase{
     }
 
     private void updatePositions() {
-        for(int i = 0; i < swerveModules.length; i++) {
+        for (int i = 0; i < swerveModules.length; i++) {
             modulePosition[i] = swerveModules[i].getPosition();
         }
     }
 
     public void stopModules() {
-        for(SwerveModule module : swerveModules) {
+        for (SwerveModule module : swerveModules) {
             module.stop();
         }
     }
 
     public void setModuleStates(SwerveModuleState[] desiredStates) {
         SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, MAX_VELOCITY_METERS_PER_SECOND);
-        for(int i = 0; i < swerveModules.length; i++) {
+        for (int i = 0; i < swerveModules.length; i++) {
             swerveModules[i].setDesiredState(desiredStates[i]);
         }
     }
 
     public void toggleModulesLocked() {
-        for(SwerveModule module : swerveModules) {
+        for (SwerveModule module : swerveModules) {
             module.toggleLocked();
         }
     }
