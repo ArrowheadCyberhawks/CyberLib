c5b29ba435a73157df17ab88abd823575b456ab5
Cyberhawks706 <robotics@arrowheadschools.org>
2025-02-03 20:58:27 -0600

    ToPoint Optimizations and New Paths

--- src/main/java/lib/frc706/cyberlib/commands/ToPointCommand.java
+++ src/main/java/lib/frc706/cyberlib/commands/ToPointCommand.java
@@ -9,19 +9,22 @@ import lib.frc706.cyberlib.subsystems.SwerveSubsystem;
 
 public class ToPointCommand extends TrackPointCommand {
     
-    private PIDController xController, yController;
+    private static PIDController xController, yController;
     private double desiredDistance;
 
     /**
      * Command to move the robot to a location on the field using the swerve drive. When this command is done, the front of the robot
      * will be facing directly into the face of the target pose.
      * @param swerveSubsystem the swerve subsystem
-     * @param targetSupplier supplies the pose we want to move to
      * @param xController the PID controller for the x axis(revolving around the target)
      * @param yController the PID controller for the y axis(distance from the target)
+     * @param turningController the PID controller for the rotation of the robot (used for angling the robot towards the target)
+     * @param targetSupplier supplies the pose we want to move to
      * @param desiredDistance the distance we want the center of the robot to be from the target. Usually the distance from center to the front face of the robot.
+     * @param maxVel the maximum velocity of the robot in m/s
+     * @param maxAngularVel the maximum angular velocity of the robot in rad/s
      */
-    public ToPointCommand(SwerveSubsystem swerveSubsystem, Supplier<Pose2d> targetSupplier, PIDController xController, PIDController yController, PIDController turningController, double desiredDistance, double maxVel, double maxAngularVel) {
+    public ToPointCommand(SwerveSubsystem swerveSubsystem, PIDController xController, PIDController yController, PIDController turningController, Supplier<Pose2d> targetSupplier, double desiredDistance, double maxVel, double maxAngularVel) {
         /*
          * everything in an explicit super constructor has to be static
          * so we have to do stupid hacks like set functions to null then reset them later
@@ -34,13 +37,32 @@ public class ToPointCommand extends TrackPointCommand {
          * what he said was 🔥 fr - nitin
          * 
          */
-        super(swerveSubsystem, targetSupplier, null, null, ()->0.0, maxVel, maxAngularVel, false, turningController);
+        super(swerveSubsystem, turningController, targetSupplier, null, null, ()->0.0, maxVel, maxAngularVel);
         super.setXSupplier(() -> {return -calculateXSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
         super.setYSupplier(() -> {return -calculateYSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
-        this.xController = xController;
-        this.yController = yController;
+        ToPointCommand.xController = xController;
+        ToPointCommand.yController = yController;
         this.desiredDistance = desiredDistance;
         //monkey ahh code
+    }
+
+    /**
+     * WARNING: ONLY USE THIS CONSTRUCTOR IF THE OTHER ONE HAS ALREADY BEEN USED AT LEAST ONCE!!!
+     * THE OTHER CONSTRUCTOR SETS STATIC VARIABLES WHICH ARE REQUIRED FOR THIS ONE TO WORK!
+     * 
+     * @param targetSupplier Supplies the pose we want to move to
+     * @param desiredDistance The distance we want the center of the robot to be from the target. Usually the distance from center to the front face of the robot.
+     */
+    public ToPointCommand(Supplier<Pose2d> targetSupplier, double desiredDistance) {
+        super(targetSupplier, null, null); // i hate this so much
+        super.setXSupplier(() -> {return -calculateXSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
+        super.setYSupplier(() -> {return -calculateYSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
+        this.desiredDistance = desiredDistance;
+        if(xController == null) {
+            throw new NullPointerException("xController is null!");
+        } else if(yController == null) {
+            throw new NullPointerException("yController is null!");
+        }
     }
 
     private double calculateXSpeed(Pose2d currentPose, Pose2d targetPose) {
@@ -77,6 +99,6 @@ public class ToPointCommand extends TrackPointCommand {
     @Override
     public boolean isFinished() {
         //robot needs to kill itself at some point
-        return super.swerveSubsystem.getPose().getTranslation().getDistance(targetSupplier.get().getTranslation()) < desiredDistance + 0.05;
+        return swerveSubsystem.getPose().getTranslation().getDistance(targetSupplier.get().getTranslation()) < desiredDistance + 0.05;
     }
 }
--- src/main/java/lib/frc706/cyberlib/commands/TrackPointCommand.java
+++ src/main/java/lib/frc706/cyberlib/commands/TrackPointCommand.java
@@ -15,32 +15,88 @@ import lib.frc706.cyberlib.subsystems.SwerveSubsystem;
 public class TrackPointCommand extends Command {
 
     protected static SwerveSubsystem swerveSubsystem;
-    private Supplier<Double> xSupplier, ySupplier, accelSupplier;
     private static PIDController m_turningController;
-
     private static double maxVel = Double.MIN_VALUE;
     private static double maxAngularVel = Double.MIN_VALUE;
+
+    private Supplier<Double> xSupplier, ySupplier, accelSupplier;
     protected Supplier<Pose2d> targetSupplier;
     private final boolean controllerCorrections;
 
-    public TrackPointCommand(SwerveSubsystem swerveSubsystem, Supplier<Pose2d> targetSupplier,
-        Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> accelFunction, double maxVel, double maxAngularVel, boolean controllerCorrections, PIDController turningController) {
+    public TrackPointCommand(SwerveSubsystem swerveSubsystem, PIDController turningController, Supplier<Pose2d> targetSupplier,
+        Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> accelFunction, double maxVel, double maxAngularVel) {
         TrackPointCommand.swerveSubsystem = swerveSubsystem;
         this.targetSupplier = targetSupplier;
         this.xSupplier = xSpdFunction;
         this.ySupplier = ySpdFunction;
         this.accelSupplier = accelFunction;
-        this.controllerCorrections = controllerCorrections;
+        this.controllerCorrections = true;
         TrackPointCommand.maxVel = maxVel;
         TrackPointCommand.maxAngularVel = maxAngularVel;
         m_turningController = turningController;
         addRequirements(swerveSubsystem);
     }
-//TODO: add more constructors so we don't have to deal with that... thing up there every single time
-    // public TrackPointCommand(Supplier<Pose2d> targetSupplier, Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> accelFunction, boolean controllerCorrections) {
-    //     this(swerveSubsystem, targetSupplier, xSpdFunction, ySpdFunction, accelFunction, maxVel, maxAngularVel, controllerCorrections, m_turningController);
-    // }
 
+    public TrackPointCommand(SwerveSubsystem swerveSubsystem, PIDController turningController, Supplier<Pose2d> targetSupplier,
+        Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, double maxVel, double maxAngularVel) {
+        TrackPointCommand.swerveSubsystem = swerveSubsystem;
+        this.targetSupplier = targetSupplier;
+        this.xSupplier = xSpdFunction;
+        this.ySupplier = ySpdFunction;
+        this.accelSupplier = ()->1.0;
+        this.controllerCorrections = false;
+        TrackPointCommand.maxVel = maxVel;
+        TrackPointCommand.maxAngularVel = maxAngularVel;
+        m_turningController = turningController;
+        addRequirements(swerveSubsystem);
+    }
+
+    /**
+     * WARNING: DO NOT USE THIS CONSTRUCTOR UNLESS AT LEAST ONE OF THE FULL CONSTRUCTORS HAS BEEN USED FIRST!!!
+     * THE OTHER CONSTRUCTORS INITIALIZE STATIC VARIABLES WHICH WILL CAUSE THIS CONSTRUCTOR TO THROW A NULL POINTER EXCEPTION IF NOT USED FIRST!
+     * 
+     * Partial constructor for TrackPointCommand. Supplying an acceleration function is optional, and will enable controller corrections if provided.
+     * 
+     * @param targetSupplier Supplies the pose we want to point towards.
+     * @param xSpdFunction Supplies the x speed of the robot in m/s.
+     * @param ySpdFunction Supplies the y speed of the robot in m/s.
+     * @param accelFunction Supplies the interpolation factor for the speed of the robot.
+     */
+    public TrackPointCommand(Supplier<Pose2d> targetSupplier, Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> accelFunction) {
+        this(swerveSubsystem, m_turningController, targetSupplier, xSpdFunction, ySpdFunction, accelFunction, maxVel, maxAngularVel);
+        if(swerveSubsystem == null) {
+            throw new NullPointerException("SwerveSubsystem is null!");
+        } else if (maxVel == Double.MIN_VALUE) {
+            throw new NullPointerException("maxVel is not set!");
+        } else if (maxAngularVel == Double.MIN_VALUE) {
+            throw new NullPointerException("maxAngularVel is not set!");
+        } else if (m_turningController == null) {
+            throw new NullPointerException("m_turningController is null!");
+        }
+    }
+
+    /**
+     * WARNING: DO NOT USE THIS CONSTRUCTOR UNLESS AT LEAST ONE OF THE FULL CONSTRUCTORS HAS BEEN USED FIRST!!!
+     * THE OTHER CONSTRUCTORS INITIALIZE STATIC VARIABLES WHICH WILL CAUSE THIS CONSTRUCTOR TO THROW A NULL POINTER EXCEPTION IF NOT USED FIRST!
+     * 
+     * Partial constructor for TrackPointCommand with controller corrections disabled.
+     * 
+     * @param targetSupplier Supplies the pose we want to point towards.
+     * @param xSpdFunction Supplies the x speed of the robot in m/s.
+     * @param ySpdFunction Supplies the y speed of the robot in m/s.
+     */
+    public TrackPointCommand(Supplier<Pose2d> targetSupplier, Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction) {
+        this(swerveSubsystem, m_turningController, targetSupplier, xSpdFunction, ySpdFunction, maxVel, maxAngularVel);
+        if(swerveSubsystem == null) {
+            throw new NullPointerException("SwerveSubsystem is null!");
+        } else if (maxVel == Double.MIN_VALUE) {
+            throw new NullPointerException("maxVel is not set!");
+        } else if (maxAngularVel == Double.MIN_VALUE) {
+            throw new NullPointerException("maxAngularVel is not set!");
+        } else if (m_turningController == null) {
+            throw new NullPointerException("m_turningController is null!");
+        }
+    }
 
     public void setXSupplier(Supplier<Double> newSupplier) {
         xSupplier = newSupplier;
