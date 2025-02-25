1de06a2498cc532f7938a6c7610dbbcf4f7f2408
Cyberhawks706 <robotics@arrowheadschools.org>
2025-02-24 20:50:30 -0600

    Changed Forwards Direction of Robot WIP

--- src/main/java/lib/frc706/cyberlib/commands/ToPointCommand.java
+++ src/main/java/lib/frc706/cyberlib/commands/ToPointCommand.java
@@ -2,109 +2,68 @@ package lib.frc706.cyberlib.commands;
 
 import java.util.function.Supplier;
 
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
 import lib.frc706.cyberlib.subsystems.SwerveSubsystem;
 
-public class ToPointCommand extends TrackPointCommand {
+public class ToPointCommand extends Command {
     
-    private static PIDController xController, yController;
-    private double desiredDistance;
+    private final SwerveSubsystem swerveSubsystem;
+    private ProfiledPIDController xController, yController, thetaController;
+    private Supplier<Pose2d> targetSupplier;
 
     /**
      * Command to move the robot to a location on the field using the swerve drive. When this command is done, the front of the robot
      * will be facing directly into the face of the target pose.
      * @param swerveSubsystem the swerve subsystem
-     * @param xController the PID controller for the x axis(revolving around the target)
-     * @param yController the PID controller for the y axis(distance from the target)
+     * @param xController the PID controller for the x axis
+     * @param yController the PID controller for the y axis
      * @param turningController the PID controller for the rotation of the robot (used for angling the robot towards the target)
      * @param targetSupplier supplies the pose we want to move to
-     * @param desiredDistance the distance we want the center of the robot to be from the target. Usually the distance from center to the front face of the robot.
-     * @param maxVel the maximum velocity of the robot in m/s
-     * @param maxAngularVel the maximum angular velocity of the robot in rad/s
      */
-    public ToPointCommand(SwerveSubsystem swerveSubsystem, PIDController xController, PIDController yController, PIDController turningController, Supplier<Pose2d> targetSupplier, double desiredDistance, double maxVel, double maxAngularVel) {
-        /*
-         * everything in an explicit super constructor has to be static
-         * so we have to do stupid hacks like set functions to null then reset them later
-         * this is a terrible way to do it but it works i guess
-         * 
-         * SCREW YOU JAVA
-         * 
-         * -grant
-         * 
-         * what he said was 🔥 fr - nitin
-         * 
-         */
-        super(swerveSubsystem, turningController, targetSupplier, null, null, ()-> 0.0, maxVel, maxAngularVel);
-        super.setXSupplier(() -> {return -calculateXSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
-        super.setYSupplier(() -> {return -calculateYSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
-        ToPointCommand.xController = xController;
-        ToPointCommand.yController = yController;
-        this.desiredDistance = desiredDistance;
-        //monkey ahh code
-    }
-
-    /**
-     * WARNING: ONLY USE THIS CONSTRUCTOR IF THE OTHER ONE HAS ALREADY BEEN USED AT LEAST ONCE!!!
-     * THE OTHER CONSTRUCTOR SETS STATIC VARIABLES WHICH ARE REQUIRED FOR THIS ONE TO WORK!
-     * 
-     * @param targetSupplier Supplies the pose we want to move to
-     * @param desiredDistance The distance we want the center of the robot to be from the target. Usually the distance from center to the front face of the robot.
-     */
-    public ToPointCommand(Supplier<Pose2d> targetSupplier, double desiredDistance) {
-        super(targetSupplier, null, null); // i hate this so much
-        super.setXSupplier(() -> {return -calculateXSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
-        super.setYSupplier(() -> {return -calculateYSpeed(swerveSubsystem.getPose(), targetSupplier.get());});
-        this.desiredDistance = desiredDistance;
-        if(xController == null) {
-            throw new NullPointerException("xController is null!");
-        } else if(yController == null) {
-            throw new NullPointerException("yController is null!");
-        }
-    }
-
-    /**
-     * Calculates the horizontal speed of the robot around a circle centered at the target pose 
-     * such that the robot will lie on the line normal to the face of the target
-     * 
-     * @param currentPose current pose of the robot
-     * @param targetPose pose of the apriltag (or whatever else we want to point towards)
-     */
-    private double calculateXSpeed(Pose2d currentPose, Pose2d targetPose) {
-        double thetaError = calculatePolarAngleTo(currentPose, targetPose); // get the distance we need to travel around the circle
-        double xSpeed = xController.calculate(thetaError, 0); // calculate the speed we need to travel with PID
-        xSpeed /= (Math.abs(TrackPointCommand.calculateAngleTo(currentPose, targetPose))+1); // slow down if we're not facing the target
-        return xSpeed;
-    }
-
-    private double calculateYSpeed(Pose2d currentPose, Pose2d targetPose) {
-        double distance = currentPose.getTranslation().getDistance(targetPose.getTranslation()); // get the distance from the target
-        double ySpeed = yController.calculate(distance, desiredDistance); // calculate the speed we need to travel with PID
-        ySpeed /= (Math.abs(TrackPointCommand.calculateAngleTo(currentPose, targetPose))+1); // slow down if we're not facing the target
-        return ySpeed;
-    }
-    /**
-     * Calculates the angle to point the robot towards the target
-     * @param currentPose current pose of the robot
-     * @param targetPose pose of the apriltag (or whatever else we want to point towards)
-     * @return angle between the robot and the vector facing into the front of the target
-     */
-    private static double calculatePolarAngleTo(Pose2d currentPose, Pose2d targetPose) {
-        Rotation2d thetaError = currentPose.getRotation().minus(targetPose.getRotation());
-                        // TODO: ?!?!?!?!?!?!?!?!?!?!??!?!
-        return thetaError.minus(Rotation2d.fromDegrees(69)).getRadians();
-    }
-
-    @Override
-    public void initialize() {
-        
+    public ToPointCommand(SwerveSubsystem swerveSubsystem, Supplier<Pose2d> targetSupplier, ProfiledPIDController xController, ProfiledPIDController yController, ProfiledPIDController turningController, double translationTolerance, double rotationTolerance) {
+        this.swerveSubsystem = swerveSubsystem;
+        this.xController = xController;
+        this.yController = yController;
+        thetaController = turningController;
+        this.xController.setTolerance(translationTolerance);
+        this.yController.setTolerance(translationTolerance);
+        thetaController.setTolerance(rotationTolerance);
+        this.targetSupplier = targetSupplier;
     }
 
     @Override
     public void execute() {
-        super.execute();
+        Pose2d currentPose = swerveSubsystem.getPose();
+        Pose2d targetPose = targetSupplier.get();
+        if (targetPose.getX() != xController.getGoal().position) {
+            xController.setGoal(targetPose.getX());
+        }
+        if (targetPose.getY() != yController.getGoal().position) {
+            yController.setGoal(targetPose.getY());
+        }
+        if (targetPose.getRotation().getRadians() != thetaController.getGoal().position) {
+            thetaController.setGoal(targetPose.getRotation().getRadians());
+        }
+        double xSpeed = xController.calculate(targetPose.getX() - currentPose.getX());
+        double ySpeed = yController.calculate(targetPose.getY() - currentPose.getY());
+        double thetaSpeed = thetaController.calculate(targetPose.getRotation().minus(currentPose.getRotation()).getRadians());
+        ChassisSpeeds speeds = new ChassisSpeeds(xSpeed, ySpeed, thetaSpeed);
+        swerveSubsystem.driveFieldOriented(speeds);
+        System.out.println("X: " + xSpeed + " Y: " + ySpeed + " Theta: " + thetaSpeed);
+    }
+
+    @Override
+    public void initialize() {
+        if (targetSupplier == null) {
+           return;
+        }
+        xController.setGoal(targetSupplier.get().getX());
+        yController.setGoal(targetSupplier.get().getY());
+        thetaController.setGoal(targetSupplier.get().getRotation().getRadians());
     }
 
     @Override
@@ -115,6 +74,6 @@ public class ToPointCommand extends TrackPointCommand {
     @Override
     public boolean isFinished() {
         //robot needs to kill itself at some point
-        return swerveSubsystem.getPose().getTranslation().getDistance(targetSupplier.get().getTranslation()) < desiredDistance + 0.05;
+        return xController.atGoal() && yController.atGoal() && thetaController.atGoal();
     }
 }
